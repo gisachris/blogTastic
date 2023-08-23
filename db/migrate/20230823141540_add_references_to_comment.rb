56f7c7e4bf0b33283c4007a7fcdec0b5572bd2a4
gisa chris <gisachris24@gmail.com>
2023-08-23 12:37:22 -0200

    Add References From Tables Through Migrations

--- db/migrate/20230823141540_add_references_to_comment.rb
+++ db/migrate/20230823141540_add_references_to_comment.rb
@@ -1,4 +1,6 @@
 class AddReferencesToComment < ActiveRecord::Migration[7.0]
   def change
+    add_reference :comments, :user, foreign_key: {to_table: :users, column: :AuthorId}
+    add_reference :comments, :posts, foreign_key: true
   end
 end
--- db/migrate/20230823141650_add_references_to_posts.rb
+++ db/migrate/20230823141650_add_references_to_posts.rb
@@ -1,4 +1,5 @@
 class AddReferencesToPosts < ActiveRecord::Migration[7.0]
   def change
+    add_reference :posts, :user, foreign_key: {to_table: :users, column: :AuthorId}
   end
 end
