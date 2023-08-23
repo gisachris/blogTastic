57044c6dc641a7368d444d857f4eb19634c08e39
gisa chris <gisachris24@gmail.com>
2023-08-23 12:43:48 -0200

    Add Indexes For Foreign Key Columns In Tables

--- db/migrate/20230823141650_add_references_to_posts.rb
+++ db/migrate/20230823141650_add_references_to_posts.rb
@@ -1,5 +1,7 @@
 class AddReferencesToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :user, foreign_key: {to_table: :users, column: :AuthorId}
+    add_reference :posts, :user, foreign_key: {to_table: :users, column: :author_id}
+
+    add_index :posts, :author_id
   end
 end
--- db/migrate/20230823141729_add_references_to_likes.rb
+++ db/migrate/20230823141729_add_references_to_likes.rb
@@ -1,6 +1,9 @@
 class AddReferencesToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :user, foreign_key: {to_table: :users, column: :AuthorId}
+    add_reference :likes, :user, foreign_key: {to_table: :users, column: :author_id}
     add_reference :likes, :post, foreign_key: true
+
+    add_index :likes, :author_id
+    add_index :likes, :post_id
   end
 end
