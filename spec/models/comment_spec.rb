36a0e77013718ae6ecc986f6f38e0d4754725e22
gisa chris <gisachris24@gmail.com>
2023-08-25 15:26:30 -0200

    Fix Linter Errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   let(:user) { User.create(name: 'Alice') }
   let(:post) { Post.create(title: 'Test Post', text: 'Testing', author: user) }
-  subject { Comment.new(text: 'Hello', author: user, post: post) }
+  subject { Comment.new(text: 'Hello', author: user, post:) }
 
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it "is not valid without text" do
+  it 'is not valid without text' do
     subject.text = nil
     expect(subject).to_not be_valid
   end
 
-  it "is associated with an author" do
+  it 'is associated with an author' do
     expect(subject.author).to eq(user)
   end
 
-  it "is associated with a post" do
+  it 'is associated with a post' do
     expect(subject.post).to eq(post)
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-  subject {User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)}
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
 
-  before {subject.save}
+  before { subject.save }
 
-  it "Validates succesfully with correct entries" do
+  it 'Validates succesfully with correct entries' do
     expect(subject).to be_valid
   end
 
@@ -13,7 +13,7 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it "Validates successfully with valid posts_counter value" do
+  it 'Validates successfully with valid posts_counter value' do
     subject.posts_counter = 5
     expect(subject).to be_valid
   end
@@ -23,13 +23,14 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it "Has an empty array of recent_posts if no posts exist" do
+  it 'Has an empty array of recent_posts if no posts exist' do
     expect(subject.recent_posts).to be_empty
   end
 
-  it "Returns recent posts when they exist" do
+  it 'Returns recent posts when they exist' do
     test_user = User.create(name: 'brian', photo: 'img', bio: 'Teacher.', posts_counter: 0)
-    Post.create(title: "Post 1", text: "testing Text for post 1", author: test_user, comments_counter: 0, likes_counter: 0)
+    Post.create(title: 'Post 1', text: 'testing Text for post 1', author: test_user, comments_counter: 0,
+                likes_counter: 0)
     test_user.reload
     expect(test_user.recent_posts.size).to eq(1)
   end
