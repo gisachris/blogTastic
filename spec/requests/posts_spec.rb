40bf2d7cb505eeb96d16dcccffd31f63eb653b7a
gisa chris <gisachris24@gmail.com>
2023-08-30 07:25:17 -0200

    Create Request Specs For Controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  let!(:author) {
+    User.create!(
+      name: 'Tom', 
+      photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+      bio: 'Teacher from Mexico.',
+      posts_counter: 0
+    )
+  }
+
+  let!(:valid_attributes){
+   { 
+    title: 'Tom \'s post', 
+    text: 'This is a post!',
+    comments_counter: 0,
+    likes_counter: 0,
+    author: author
+   }
+  }
+
+  let!(:invalid_attributes){
+    { 
+      title: '', 
+      text: 'This is a post!'
+   }
+  } 
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "loads the index page succesfully with correct attributes" do
+      Post.create! valid_attributes
+      get user_posts_url(author)
+      expect(response).to be_successful
+    end
+
+    it "Has the correct status" do
+      Post.create! valid_attributes
+      get user_posts_url(author)
+      expect(response.status).to eq(200)
+    end
+
+    it "Loads the correct template view" do
+      Post.create! valid_attributes
+      get user_posts_url(author)
+      expect(response).to render_template(:index)
+    end
+
+    it "Has the correct placeholder text" do
+      Post.create! valid_attributes
+      get user_posts_url(author)
+      expect(response.body).to include ('Here is a list of all posts for a given user')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'loads the specific Post' do
+      post = Post.create! valid_attributes
+      get user_post_url(author,post)
+      expect(response).to be_successful
+    end
+
+    it "Has the correct status" do
+      post = Post.create! valid_attributes
+      get user_post_url(author,post)
+      expect(response.status).to eq(200)
+    end
+
+    it "Loads the correct template view" do
+      post = Post.create! valid_attributes
+      get user_post_url(author,post)
+      expect(response).to render_template(:show)
+    end
+
+    it "Has the correct placeholder text" do
+      post = Post.create! valid_attributes
+      get user_post_url(author,post)
+      expect(response.body).to include ('Here is a specific post for a given user')
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+
+  let!(:valid_attributes){
+   { 
+    name: 'Tom', 
+    photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+    bio: 'Teacher from Mexico.',
+    posts_counter: 0
+   }
+  }
+
+  let!(:invalid_attributes){
+    { 
+    name: '', 
+    photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+    bio: 'Teacher from Mexico.'
+   }
+  }
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "loads the index page succesfully with correct attributes" do
+      User.create! valid_attributes
+      get users_url
+      expect(response).to be_successful
+    end
+
+    it "Has the correct status" do
+      User.create! valid_attributes
+      get users_url
+      expect(response.status).to eq(200)
+    end
+
+    it "Loads the correct template view" do
+      User.create! valid_attributes
+      get users_url
+      expect(response).to render_template(:index)
+    end
+
+    it "Has the correct placeholder text" do
+      User.create! valid_attributes
+      get users_url
+      expect(response.body).to include ('Welcome to the homepage')
+    end
   end
+
+  describe 'GET /show' do
+    it 'loads the specific user' do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response).to be_successful
+    end
+
+    it 'redirects to the created user' do
+    end
+
+    it "Has the correct status" do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response.status).to eq(200)
+    end
+
+    it "Loads the correct template view" do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response).to render_template(:show)
+    end
+
+    it "Has the correct placeholder text" do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response.body).to include ('Here is a specific user')
+    end
+  end
+
+
 end
