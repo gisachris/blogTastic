bf832ca91b0b0b56cbd026dc6ce1ef9694135520
gisa chris <gisachris24@gmail.com>
2023-08-30 07:34:20 -0200

    Fix Linter Errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,47 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let!(:valid_attributes) do
+    {
+      name: 'Tom',
+      photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+      bio: 'Teacher from Mexico.',
+      posts_counter: 0
+    }
+  end
 
-  let!(:valid_attributes){
-   { 
-    name: 'Tom', 
-    photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-    bio: 'Teacher from Mexico.',
-    posts_counter: 0
-   }
-  }
-
-  let!(:invalid_attributes){
-    { 
-    name: '', 
-    photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-    bio: 'Teacher from Mexico.'
-   }
-  }
+  let!(:invalid_attributes) do
+    {
+      name: '',
+      photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+      bio: 'Teacher from Mexico.'
+    }
+  end
 
   describe 'GET /index' do
-    it "loads the index page succesfully with correct attributes" do
+    it 'loads the index page succesfully with correct attributes' do
       User.create! valid_attributes
       get users_url
       expect(response).to be_successful
     end
 
-    it "Has the correct status" do
+    it 'Has the correct status' do
       User.create! valid_attributes
       get users_url
       expect(response.status).to eq(200)
     end
 
-    it "Loads the correct template view" do
+    it 'Loads the correct template view' do
       User.create! valid_attributes
       get users_url
       expect(response).to render_template(:index)
     end
 
-    it "Has the correct placeholder text" do
+    it 'Has the correct placeholder text' do
       User.create! valid_attributes
       get users_url
-      expect(response.body).to include ('Welcome to the homepage')
+      expect(response.body).to include('Welcome to the homepage')
     end
   end
 
@@ -52,27 +51,22 @@ RSpec.describe 'Users', type: :request do
       expect(response).to be_successful
     end
 
-    it 'redirects to the created user' do
-    end
-
-    it "Has the correct status" do
+    it 'Has the correct status' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response.status).to eq(200)
     end
 
-    it "Loads the correct template view" do
+    it 'Loads the correct template view' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response).to render_template(:show)
     end
 
-    it "Has the correct placeholder text" do
+    it 'Has the correct placeholder text' do
       user = User.create! valid_attributes
       get user_url(user)
-      expect(response.body).to include ('Here is a specific user')
+      expect(response.body).to include('Here is a specific user')
     end
   end
-
-
 end
