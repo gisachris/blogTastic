37d51ab01e4b21d41c47eaf5cf616e3ddc5c1f98
gisa chris <gisachris24@gmail.com>
2023-09-01 16:27:57 -0200

    Make Forms For Posts And Comments Creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,45 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts
     @recent_comments_by_post = @posts.to_h { |post| [post.id, post.recent_comments] }
+
+    @first_user = User.first
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @recent_comments = @post.recent_comments
+    @new_comment = @post.comments.build
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+  end  
+
+  # def new
+  #   @post = Post.new
+  #   @first_user = ApplicationController.new.current_user
+  # end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+  
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end  
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users, only: [:index, :new, :create, :show, :update] do
-    resources :posts, only: [:index, :new, :create, :show, :update]
+  resources :users do
+    resources :posts do
+      resources :comments, only: [:create, :update, :destroy]
+      resources :likes, only: [:create, :destroy]
+    end
   end
 end
