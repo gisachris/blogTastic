39d6fb082a6732e321386b36e4f67156544e9c6b
gisa chris <gisachris24@gmail.com>
2023-09-01 22:55:35 -0200

    Fix Bug With Posts Counter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,13 @@ class CommentsController < ApplicationController
   before_action :find_post
 
   def new
-    @comment = @post.comments.new
+    @comment = Comment.new
   end
 
   def create
-    @comment = @post.comments.new(comment_params)
+    @comment = Comment.new(comment_params)
     @comment.author = @user
+    @comment.post = @post
     if @comment.save
       flash[:notice] = 'Comment created successfully.'
       redirect_to user_post_path(@user, @post)
